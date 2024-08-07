0a334e79e273b8b7491dd518695586e4651abca4
iahgo <iahgo@icloud.com>
2024-08-07 20:55:55 -0300

    inicio do projeto

---
+++ src/main/java/com/iahgo/API_milhas/ApiMilhasApplication.java
@@ -0,0 +1,13 @@
+package com.iahgo.API_milhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMilhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMilhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iahgo/API_milhas/ApiMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iahgo.API_milhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
